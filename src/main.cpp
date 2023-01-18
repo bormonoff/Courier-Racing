965603cccdd10de77ed24a190d54b9ec0beb9776
Bormon <botmon_off@protonmail.com>
2023-01-18 21:41:23 +0300

    main file changed

--- src/main.cpp
+++ src/main.cpp
@@ -8,50 +8,6 @@
 
 #include "http_server.h"
 
-namespace {
-namespace net = boost::asio;
-using namespace std::literals;
-namespace sys = boost::system;
-namespace http = boost::beast::http;
-namespace beast = boost::beast;
-
-using StringRequest = http::request<http::string_body>;
-using StringResponse = http::response<http::string_body>;
-
-struct ContentType {
-    ContentType() = delete;
-    constexpr static std::string_view TEXT_HTML = "text/html"sv;
-};
-
-StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
-                                  bool keep_alive,
-                                  std::string_view content_type = ContentType::TEXT_HTML) {
-    StringResponse response(status, http_version);
-    response.set(http::field::content_type, content_type);
-    response.body() = body;
-    response.content_length(body.size());
-    response.keep_alive(keep_alive);
-    return response;
-}
-
-StringResponse HandleRequest(StringRequest&& req) {
-    const auto text_response = [&req](http::status status, std::string_view text) 
-    {
-        return MakeStringResponse(status, text, req.version(), req.keep_alive());
-    };
-
-    beast::string_view a = req.target();
-    a.remove_prefix(1);
-    std::string body = {"Hello, " + static_cast<std::string>(a)};
-    if(req.method()== http::verb::get || req.method()== http::verb::head){
-        return text_response(http::status::ok, body);
-    }else{
-        return text_response(http::status::method_not_allowed, "Invalid method");
-    }
-   
-}
-
-
 template <typename Fn>
 void RunWorkers(unsigned n, const Fn& fn) {
     n = std::max(1u, n);
@@ -62,8 +18,6 @@ void RunWorkers(unsigned n, const Fn& fn) {
     }
     fn();
 }
-
-}  // namespace
 
 int main() {
     const unsigned num_threads = std::thread::hardware_concurrency();
